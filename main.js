9484db39d14dfa02d6b6f95e28e140c5ea5a3701
nikhilkumar031982 <91476583+nikhilkumar031982@users.noreply.github.com>
2022-05-03 11:39:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//teachablemachine.withgoogle.com/models/LSTqeQLd3
+
+prediction1="";
+prediction2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+camera=document.getElementById(camera);
+Webcam.attach("#camera");
+
+ function take_snapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='capture_image' src='"+data_uri+"'/>";
+
+});
+ }
+ 
+  
